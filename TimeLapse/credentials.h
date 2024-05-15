2f783196be11d67d9652c9168e37ffb7a00a31e0
meaningofstuff.blogspot.com <mihvoi@gmail.com>
2024-05-16 01:24:42 +0300

    Add credentials.h file

---
+++ TimeLapse/credentials.h
@@ -0,0 +1,7 @@
+
+//This is protected by .gitignore and should not be commited accidentally
+//You can also use:  git update-index --skip-worktree TimeLapse/credentials.h
+
+// !!!!!! This should not be commited to GIT !!!!!!
+#define WIFI_SSID "yourWiFiSSID"
+#define WIFI_PASS "yourWiFiPassword"
