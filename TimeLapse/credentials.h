44044234aeed60790639faa54128141e59018239
meaningofstuff.blogspot.com <mihvoi@gmail.com>
2024-05-16 01:19:48 +0300

    Remove git TimeLapse/credentials.h from tracked files

--- TimeLapse/credentials.h
+++
@@ -1,6 +0,0 @@
-
-//This is protected by .gitignore and should not be commited accidentally
-
-//This should not commited to GIT !!!!!!!!!!!!!!!
-#define WIFI_SSID "yourWiFiSSID"
-#define WIFI_PASS "yourWiFiPassword2"
